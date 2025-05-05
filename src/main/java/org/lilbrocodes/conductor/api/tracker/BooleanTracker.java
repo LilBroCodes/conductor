102696f65874208f8204ef7a3050a674a0ad1547
LilBroCodes <lilbrocodes@gmail.com>
2025-05-05 22:52:22 +0200

    Quick bug fix

--- src/main/java/org/lilbrocodes/conductor/api/tracker/BooleanTracker.java
+++ src/main/java/org/lilbrocodes/conductor/api/tracker/BooleanTracker.java
@@ -3,8 +3,14 @@ package org.lilbrocodes.conductor.api.tracker;
 import java.lang.Boolean;
 
 public class BooleanTracker<T> extends AbstractTracker<T, Boolean> {
+    public BooleanTracker() {
+        setDefault(false);
+    }
+
     @Override
-    public void modifyTrackedValues() {}
+    public void modifyTrackedValues() {
+
+    }
 
     public void flip(T key) {
         put(key, Boolean.FALSE.equals(get(key)));
